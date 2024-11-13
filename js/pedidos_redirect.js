79c9270930373637359998a6b096649d3b3d2f26
Danilo <Danilo@gavari.com>
2024-11-13 15:50:50 -0300

    Pedidos Terminado

---
+++ js/pedidos_redirect.js
@@ -0,0 +1,7 @@
+document.getElementById("pedidos_pendientes").addEventListener('click', function() {
+window.location.href = 'http://127.0.0.1:5500/pedidos.html';
+});
+
+document.getElementById("pedidos_finalizados").addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/pedidos_finalizados.html';
+});
