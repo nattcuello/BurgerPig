406100c74cc127fa1397346a331e60c02b82fe07
natalia cuello <n.cuello@itecriocuarto>
2024-11-13 22:10:16 -0300

    mas cambios

--- js/redirecciones.js
+++ js/redirecciones.js
@@ -1,4 +1,11 @@
-document.getElementById('accept-second-popup').addEventListener('click', function() {
+document.getElementById('botonAceptarcocina').addEventListener('click', function() {
     // Redirige a la página del juego
-window.location.href = 'http://127.0.0.1:5500/confirmaciones1.html';
+window.location.href = 'http://127.0.0.1:5500/cocina_listos.html';
 });
+
+document.getElementById('botonAceptaragregarcarrito').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = 'http://127.0.0.1:5500/forma_pago.html';
+});
+
+
